0affc6781be9ec4d5080f7766397120a03dd0273
valenMarino <valemarino1694@gmail.com>
2023-04-05 10:26:20 -0300

    add frontend app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,18 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 app.get('/', (req: Request, res: Response) => {
-    res.send("Its working")
+
+    let oceans = [
+        {
+            Name:"Indian Ocean"
+        },
+        {
+            Name:"Atlantic Ocean"
+        }
+    ]
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify(oceans, null, 3));
 })
 
 app.listen(PORT, () => {
